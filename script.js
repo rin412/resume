369cc67a738b859f52bb9a73ed3e6fc2783b3c9a
rin412 <160198813+rin412@users.noreply.github.com>
2024-03-07 14:14:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//button pindah halaman
+function home() {
+    window.location.href = "/index.html"
+}
+function aboutMe() {
+    window.location.href = "/page/aboutme.html"
+}
+function project() {
+    window.location.href = "/page/project.html"
+}
+function contact() {
+    window.location.href = "/page/contact.html"
+}
